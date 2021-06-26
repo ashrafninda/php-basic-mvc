2cb192f6ea2093d01d62c2f066d6059e271fafaf
Ashraf Latif <ashraf.latif@nindatech.com>
2021-06-27 04:05:02 +0800

    Reenable db connection

--- views/index.view.php
+++ views/index.view.php
@@ -2,4 +2,14 @@
 
 <h1>My Tasks</h1>
 
+<?php foreach ($tasks as $task): ?>
+    <li>
+        <?php if ($task->completed) : ?>
+            <strike><?= $task->description; ?></strike>
+        <?php else: ?>
+            <?= $task->description; ?>
+        <?php endif; ?>
+    </li>
+<?php endforeach; ?>
+
 <?php require('partials/footer.php'); ?>
